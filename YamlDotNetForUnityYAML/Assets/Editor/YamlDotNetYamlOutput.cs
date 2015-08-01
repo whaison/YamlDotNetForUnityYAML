063958fe31d16513c2f80b8db97c7722c9b6e06f
whaison <whaison0422@gmail.com>
2015-08-02 04:57:27 +0900

    firstdata

---
+++ YamlDotNetForUnityYAML/Assets/Editor/YamlDotNetYamlOutput.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using YamlDotNet;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+
+
+
+public class Content
+{
+	public int Day { get; set; }
+	public string Author { get; set; }
+	public string Description { get; set; }
+	public string URL { get; set;}   
+
+}
+
+
+public class AdventCalendar
+{
+	public DateTime Year { get; set; }
+	public string Description { get; set; }
+	public List<Content> Contents { get; set; }
+
+	public AdventCalendar() {
+		this.Contents = new List<Content>();
+	}
+
+	public AdventCalendar(List<Content> contents) {
+		this.Contents = contents;
+	}
+}
+
+public class YamlDotNetYamlOutput : EditorWindow {
+
+
+	GameObject prefabAsset;
+
+	void OnGUI ()
+	{
+
+		GUILayout.Label ("YamlDotNetYamlOutput", EditorStyles.boldLabel);
+
+		/*
+		GUILayout.Space (10f);
+		prefabAsset = EditorGUILayout.ObjectField ("prefab", prefabAsset, typeof(GameObject), false) as GameObject;
+		if (prefabAsset == null) {
+			GUILayout.Label ("Set GameObject prefab or other prefab object.");
+		}
+*/
+
+
+
+		GUILayout.Space (20f);
+		if (GUILayout.Button ("YamlDotNetYamlOutput", GUILayout.Width (300f))) {
+			YamlDotNetYamlOut();
+		}
+
+	}
+
+
+	void YamlDotNetYamlOut()
+	{
+
+		//シリアライズするオブジェクトを定義する
+		var calendar = new AdventCalendar();
+		calendar.Year = new DateTime(2012, 12, 1);
+		calendar.Description = "C# Advent Calender 2012";
+		calendar.Contents.Add(new Content()  {
+			Day = 1,
+			Author = "gushwell",
+			Description = ".NET Framework4.5 での非同期I/O処理 事始め",
+			URL = @"http://gushwell.ldblog.jp/archives/52290230.html"
+            });
+		calendar.Contents.Add(new Content() {
+			Day = 2,
+			Author = "KTZ",
+			Description = "年末の大掃除",
+			URL = @"http://ritalin.github.com/2012/12-02/csharp-advent-2012/"
+            });
+		calendar.Contents.Add(new Content() {
+			Day = 3,
+			Author = "neuecc",
+			Description = "MemcachedTranscoder – C#のMemcached用シリアライザライブラリ",
+			URL = @"http://neue.cc/2012/12/03_389.html"
+            });
+
+
+		TextWriter textWriter = new StreamWriter(Application.dataPath + "/Editor/YamlDotNetYamlOutput_yaml.yaml");
+		//YAMLにシリアライズしてコンソールに表示
+		//var serializer = new Serializer();
+		YamlDotNet.Serialization.Serializer serializer = new YamlDotNet.Serialization.Serializer();
+		/// ※↑でSerializerが存在しないと怒られたら以下ので Genericsを書かないと空のYAMLになるので注意
+		//var serializer = new YamlSerializer<AdventCalendar>();
+		Debug.Log("calendar="+calendar);
+		serializer.Serialize(textWriter,calendar);
+
+		// don't forget Close()
+		textWriter.Close();
+		AssetDatabase.Refresh();
+		//Console.Read();
+	}
+	// Use this for initialization
+
+
+
+
+
+	////////////////////////////////////////////////////////////////////////////////
+
+	#region Static
+	/// <summary>
+	/// Open the tool window
+	/// </summary>
+	[MenuItem("Tools/GameObject/YamlDotNetYamlOutput")]
+	static public void OpenWindow12 ()
+	{
+		EditorWindow.GetWindow<YamlDotNetYamlOutput> (true, "YamlDotNetYamlOutput", true);
+	}
+	#endregion
+}
---
+++ YamlDotNetForUnityYAML/Assets/Editor/YamlDotNetYamlReader.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using YamlDotNet;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+
+
+public class YamlDotNetYamlReader : EditorWindow {
+
+	UnityEngine.Object textAsset;
+	GameObject prefabAsset;
+
+	void OnGUI ()
+	{
+
+		GUILayout.Label ("YamlDotNetYamlReader", EditorStyles.boldLabel);
+
+		GUILayout.Space (10f);
+		textAsset = EditorGUILayout.ObjectField ("YAML Object", textAsset, typeof(UnityEngine.Object), false) as UnityEngine.Object;
+		if (textAsset == null) {
+			GUILayout.Label ("Set YAML Data. Extension need .YAML Object");
+		}
+		/*
+		GUILayout.Space (10f);
+		prefabAsset = EditorGUILayout.ObjectField ("prefab", prefabAsset, typeof(GameObject), false) as GameObject;
+		if (prefabAsset == null) {
+			GUILayout.Label ("Set GameObject prefab or other prefab object.");
+		}
+		*/
+
+
+
+		GUILayout.Space (20f);
+		if (GUILayout.Button ("YamlDotNetYamlReader", GUILayout.Width (300f))) {
+			YamlDotNetYamlRead();
+		}
+
+	}
+
+
+	void YamlDotNetYamlRead()
+	{
+
+		//textAsset
+		string fileName=AssetDatabase.GetAssetPath(textAsset);
+		// open
+		var input = new StreamReader(fileName, Encoding.UTF8);
+		var yaml = new YamlStream();
+		yaml.Load(input);
+		var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+		Debug.Log("yaml.Documents.Count="+ yaml.Documents.Count);
+		//foreach (YamlMappingNode item in (YamlMappingNode)yaml.Documents) {
+		for(int i = 0; i < yaml.Documents.Count; i++){
+
+			string str="";
+			//str="(YamlMappingNode)yaml.Documents["+i+"].RootNode= ";
+			str=str+(YamlMappingNode)yaml.Documents[i].RootNode+"\n";//Macの場合 optionキーを押しながら¥を押す
+			Debug.Log(str);
+			//for(int j = 0; j < yaml.Documents[i].AllNodes.Count(); j++){
+				//string str1="";
+				//str="(YamlMappingNode)yaml.Documents["+i+"].RootNode= ";
+			//	str1=str1+(YamlDotNet.RepresentationModel.YamlNode)yaml.Documents[i].AllNodes[j]+"\n";//Macの場合 optionキーを押しながら¥を押す
+			//}
+			foreach (YamlDotNet.RepresentationModel.YamlNode child in yaml.Documents[i].AllNodes) {
+				Debug.Log(child.ToString());
+			}
+			//foreach (var child in item) {
+			//	Debug.Log(((YamlScalarNode)child.Key).Value + "\t" +
+			//		((YamlScalarNode)child.Value).Value);
+			//}
+			
+		}
+		//var Year = (YamlScalarNode)mapping.Children[new YamlScalarNode("Year")];
+		//Debug.Log("Year "+ Year.Value);
+		//var Description = (YamlScalarNode)mapping.Children[new YamlScalarNode("Description")];
+		//Debug.Log("Desciption "+ Description.Value);
+		//Debug.Log("Contents:");
+		
+		//var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("Contents")];
+		//foreach (YamlMappingNode item in items) {
+		//	foreach (var child in item) {
+		//		Debug.Log(((YamlScalarNode)child.Key).Value + "\t" +
+		//			((YamlScalarNode)child.Value).Value);
+		//	}
+		//}
+		
+	}
+
+
+
+
+
+
+	////////////////////////////////////////////////////////////////////////////////
+
+	#region Static
+	/// <summary>
+	/// Open the tool window
+	/// </summary>
+	[MenuItem("Tools/GameObject/YamlDotNetYamlReader")]
+	static public void OpenWindow13 ()
+	{
+		EditorWindow.GetWindow<YamlDotNetYamlReader> (true, "YamlDotNetYamlReader", true);
+	}
+	#endregion
+}
